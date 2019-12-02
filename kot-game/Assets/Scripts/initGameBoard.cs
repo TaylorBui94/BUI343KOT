0d8fd01aad1734e0e16a3f3e115899ec84730104
peterbuoy <peterbuoy@gmail.com>
2019-12-02 07:54:00 -0800

    creation of players, need to implement instantiation on board

--- kot-game/Assets/Scripts/initGameBoard.cs
+++ kot-game/Assets/Scripts/initGameBoard.cs
@@ -1,14 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class initGameBoard : MonoBehaviour
-{
+{   
+    public GameObject[] players;
+    public GameObject[] playerPrefabs;
+    public Vector3[] playerLocations;
     // Start is called before the first frame update
-    public void initPlayers() // initialize all players, resources, cards, die, etc
-    {
-        int playercount = numPlayers.playercount;
-        Debug.Log("inside Start() of initGameboard");
+    void Start() {
+        Debug.Log("Active Scene : " + SceneManager.GetActiveScene().name);
+        int playercount = numPlayers.playercount; // grab dropdown player count
+            Debug.Log("playercount is " + playercount); // inside check
+        // load all player prefabs into player array
+        GameObject[] player=Resources.LoadAll("PlayerPrefabs") as GameObject[];
+        // uh maybe check if anything is being loaded into player array
+        // initialize player locations
+        Vector3[] playerLocations = new[] {new Vector3(-354f,125f,0f),
+                                            new Vector3(-91f,118f,0f),
+                                            new Vector3(177f,120f,0f),
+                                            new Vector3(-359f,-132f,0f),
+                                            new Vector3(-90f,-132f,0f),
+                                            new Vector3(185f,-129f,0f)};      
+         
+        for(int i = 0; i < playercount; i++) {
+            // use Quaternion.identity for no rotation
+            Debug.Log("playercount in instantiate is " + playercount); // inside check
+            Debug.Log("inside for loop" + i);
+            Instantiate(player[i], playerLocations[i], Quaternion.identity);
+            Debug.Log("after instantiate " + i);
+        }
     }
 
     // Update is called once per frame
---
+++ kot-game/Assets/Scripts/playerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    public int HP = 20; // health points
+    public int VP = 0; // victory points
+    public int energy = 0;
+    // more attributes needed upon card implementation
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
